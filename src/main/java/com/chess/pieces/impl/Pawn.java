ec5b8a15ed7f4d4222a63e67872b7aeb917fc689
Kiran Gohokar <KGohokar@wsgc.com>
2020-05-31 03:14:48 -0700

    Added: Pawn possible move

--- src/main/java/com/chess/pieces/impl/Pawn.java
+++ src/main/java/com/chess/pieces/impl/Pawn.java
@@ -2,7 +2,9 @@ package com.chess.pieces.impl;
 
 import com.chess.pieces.Piece;
 import com.chess.position.Position;
+import com.chess.position.PositionMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Piece {
@@ -13,7 +15,15 @@ public class Pawn extends Piece {
 
   @Override
   public List<String> getPossibleMoves(final Position position) {
-    return null;
+    List<String> possibleMoves = new ArrayList<>();
+    int row = position.getRow();
+    int col = position.getCol();
+    Position destination = new Position(row, col + 1);
+    if (validate(position, destination)) {
+      String location = PositionMapper.getLocation(destination);
+      possibleMoves.add(location);
+    }
+    return possibleMoves;
   }
 
 }
