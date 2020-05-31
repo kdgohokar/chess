8c452e1c88224512630d682b24c0198b905f26a7
Kiran Gohokar <KGohokar@wsgc.com>
2020-05-31 03:51:15 -0700

    Added: Rook possible move

--- src/main/java/com/chess/pieces/impl/Rook.java
+++ src/main/java/com/chess/pieces/impl/Rook.java
@@ -2,7 +2,9 @@ package com.chess.pieces.impl;
 
 import com.chess.pieces.Piece;
 import com.chess.position.Position;
+import com.chess.position.PositionMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece {
@@ -13,7 +15,43 @@ public class Rook extends Piece {
 
   @Override
   public List<String> getPossibleMoves(final Position position) {
-    return null;
+    List<String> possibleMoves = new ArrayList<>();
+    int row = position.getRow();
+    int col = position.getCol();
+
+    for (int i = row + 1; i < 8; i++) {
+      Position destination = new Position(i, col);
+      if (validate(position, destination)) {
+        final String location = PositionMapper.getLocation(destination);
+        possibleMoves.add(location);
+      }
+    }
+
+    for (int i = row - 1; i >= 0; i--) {
+      Position destination = new Position(i, col);
+      if (validate(position, destination)) {
+        final String location = PositionMapper.getLocation(destination);
+        possibleMoves.add(location);
+      }
+    }
+
+    for (int i = col + 1; i < 8; i++) {
+      Position destination = new Position(row, i);
+      if (validate(position, destination)) {
+        final String location = PositionMapper.getLocation(destination);
+        possibleMoves.add(location);
+      }
+    }
+
+    for (int i = col - 1; i >= 0; i--) {
+      Position destination = new Position(row, i);
+      if (validate(position, destination)) {
+        final String location = PositionMapper.getLocation(destination);
+        possibleMoves.add(location);
+      }
+    }
+
+    return possibleMoves;
   }
 
 }
